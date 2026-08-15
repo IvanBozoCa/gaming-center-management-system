e6cb9c47f4ecede01879e2193eae27507c0e13b4
IvanBozoCa <133916421+IvanBozoCa@users.noreply.github.com>
2026-08-15 13:27:01 -0400

    SCRUM-44 feat(admin): add time purchase endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from app.api.routes.auth import router as auth_router
 from app.api.routes.me import router as me_router
+from app.api.routes.admin import (
+    router as admin_router,
+)
 
 app = FastAPI(
     title="Gaming Center Management System API",
@@ -10,6 +13,7 @@ app = FastAPI(
 
 app.include_router(auth_router)
 app.include_router(me_router)
+app.include_router(admin_router)
 
 @app.get("/health")
 def health_check():
